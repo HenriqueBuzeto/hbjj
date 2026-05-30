37eb5c1890e540d8014c26492820d9d55cb3ed33
HenriqueBuzeto <henriquebv.vieirads@gmail.com>
2026-05-29 21:05:10 -0300

    fix: update middleware to use new simple authentication cookie

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
 export function middleware(request: NextRequest) {
-  const token = request.cookies.get('next-auth.session-token') || request.cookies.get('__Secure-next-auth.session-token')
+  const token = request.cookies.get('user_session')
   const isLoggedIn = !!token
   
   const isOnDashboard = request.nextUrl.pathname.startsWith('/dashboard')
